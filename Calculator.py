8de261a988bebd72f4954b43f1ab05caf7580545
xhxushush <116039975+xhxushush@users.noreply.github.com>
2022-10-17 18:23:05 -0400

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,15 @@
+print("Simple Calculator of 20 and 10: ")
+x = 20
+y = 10
+
+sum = x+y
+differance= x-y
+product = x*y
+quotient = x/y
+remainder = x%y
+
+print("The Sum is",sum)
+print("The Differance is",differance)
+print("The Product is",product)
+print("The Quotient is",quotient)
+print("The Remainder is",remainder)
